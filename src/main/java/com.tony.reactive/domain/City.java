f387af283e5a7ed7d8a380cb4c2fa3518322384e
Administrator <51616330@qq.com>
2019-07-21 00:07:09 +0800

    create data transfer object

---
+++ src/main/java/com.tony.reactive/domain/City.java
@@ -0,0 +1,50 @@
+package com.tony.reactive.domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+/**
+ * @author tony
+ * @describe City
+ * @date 2019/06/30
+ */
+public class City implements Serializable {
+    @Id
+    private Long id;
+    private Long provinceId;
+    private String cityName;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(Long provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
